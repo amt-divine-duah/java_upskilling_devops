a2f52229247659a6c9546cc6835fa0c9290a3f7c
Divine Duah <divine.duah@amalitech.com>
2024-04-02 11:57:48 +0000

    intro to maven

---
+++ intro_to_maven/src/main/java/com/intro_to_maven/IntroToMavenApplication.java
@@ -0,0 +1,13 @@
+package com.intro_to_maven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroToMavenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntroToMavenApplication.class, args);
+    }
+
+}
